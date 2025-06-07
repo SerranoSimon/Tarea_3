ca7fcc39ff5530cbb6ae985d03cb59bd1988f508
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-06-07 14:46:37 -0400

    documentacion javadoc class PanelDepositoMonedas

--- src/Visual/PanelDepositoMonedas.java
+++ src/Visual/PanelDepositoMonedas.java
@@ -8,14 +8,28 @@ import Logica.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ *Clase que representa visualmente el deposito de monedas de un expendedor
+ * muestra las monedas disponibles como iconos pequeños y permite la actualizacion de su visualizacion dependiendo de los eventos.
+ */
 public class PanelDepositoMonedas extends JPanel {
     private DepositoGenerico<Moneda> depositoMonedas;
+
+    /**
+     *constructor que inicia el panel.
+     * @param exp expendedor del cual se crea el panel de deposito de monedas.
+     */
     public PanelDepositoMonedas(Expendedor exp){
         this.setBackground(Color.WHITE);
         this.setLayout(new FlowLayout());
         this.depositoMonedas=exp.getDepositoMonedas();
 
     }
+
+    /**
+     *metodo para actualizar visualmente el deposito de monedas.
+     * las ordena y las muestra en un tamaño mas pequeño.
+     */
     public void actualizar() {
          this.removeAll();
         Collections.sort(depositoMonedas.getArr());
