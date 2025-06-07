9d54f636c6fb8ce9e7ba95b143540175a81d2b53
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-06-07 15:50:17 -0400

    documentacion javadoc class Ventana

--- src/Visual/Ventana.java
+++ src/Visual/Ventana.java
@@ -4,28 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 import Logica.*;
 
+/**
+ * Clase que crea una ventana para la visibilidad de la aplicación.
+ */
 public class Ventana extends JFrame {
+
     private PanelPrincipal panelPrincipal;
+
+    /**
+     * Crea una nueva ventana con su configuración específica y resolución.
+     * Añade el panelPrincipal y lo hace visible, lo cual muestra toda la funcionalidad de la aplicación.
+     */
     public Ventana(){
         super();
         this.setLayout(new BorderLayout());
-        /*this.snickers=new Snickers(200) ;
-        //panelPrincipal=new PanelPrincipal();
-        this.setTitle("Ventana");
-        JLabel label=new JLabel("foto:");
-        label.setIcon(snickers.getIcon());*/
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(1920,1080);
         this.setResizable(false);
         this.panelPrincipal=new PanelPrincipal();
         this.add(panelPrincipal);
-
-
-
-
-
-
-       // this.add(panelPrincipal);
         this.setVisible(true);
     }
 }
