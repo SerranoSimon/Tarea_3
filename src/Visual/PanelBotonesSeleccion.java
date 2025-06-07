ca6b992f4131443e0ddcefe310218b030e992426
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-06-07 14:37:25 -0400

    documentacion javadoc class PanelBotonesSeleccion

--- src/Visual/PanelBotonesSeleccion.java
+++ src/Visual/PanelBotonesSeleccion.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Clase de tipo Panel que representa los botones de seleccion de productos de un expendedor, enumerados del 1 al 5.
+ */
 public class PanelBotonesSeleccion extends JPanel {
+    /**
+     * constructor del panel que crea los botones con cierta configuracion.
+     */
     private ArrayList<JButton> botonesSeleccion;
     public PanelBotonesSeleccion(){
         this.setLayout(new FlowLayout());
@@ -19,6 +25,10 @@ public class PanelBotonesSeleccion extends JPanel {
         }
     }
 
+    /**
+     * metodo para obtener los botones de seleccion.
+     * @return lista con los botones de seleccion de un expendedor.
+     */
     public ArrayList<JButton> getBotonesSeleccion() {
         return botonesSeleccion;
     }
