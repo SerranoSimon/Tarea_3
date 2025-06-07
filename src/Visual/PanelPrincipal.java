64f135d51c65d6af4de9602a526bd1a9425339d2
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-06-07 15:17:14 -0400

    documentacion javadoc class PanelPrincipal

--- src/Visual/PanelPrincipal.java
+++ src/Visual/PanelPrincipal.java
@@ -2,11 +2,20 @@ package Visual;
 import Logica.*;
 import java.awt.*;
 import javax.swing.*;
+
+/**
+ * Clase que actua como panel principal, utiliza el panelComprador y panelExpendedor para dar una interfaz grafica que permite la interaccion entre ambos paneles, representando la compra y funcionamiento de un expendedor.
+ * Actua como contenedor principal de la interfaz grafica de la aplicacion.
+ */
 public class PanelPrincipal extends JPanel{
 
     private PanelExpendedor panelExpendedor;
     private PanelComprador panelComprador;
 
+    /**
+     *Constructor que inicializa el panel principal
+     * Crea y configura los paneles de expendedor y comprador con sus respectivos tamaños y resolución.
+     */
     public PanelPrincipal(){
         super();
         Expendedor exp=new Expendedor(6);
